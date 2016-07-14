8f98c8d63f8929268be72fa8d57aecba4167a70f
vlad <uplight.ca@gmail.com>
2016-07-14 12:54:44 -0400

    marge

--- client/app/contentmanager/contentmanager.ts
+++ client/app/contentmanager/contentmanager.ts
@@ -21,9 +21,7 @@ import { addRoutes } from "../add/add.routes";
                 </div>
                 <div *ngIf="isAddContent">
                   <div id="myModal" class="modal" role="dialog">
-                      <div class="modal-dialog">
-         
-                        <!-- Modal content-->
+                      <div class="modal-dialog">                      
                         <div class="modal-content">
                           <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" (click)="onModalClose()">&times;</button>
