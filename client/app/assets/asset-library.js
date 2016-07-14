f749c5c91d21994c69991733c9d668f932b3e50d
vlad <uplight.ca@gmail.com>
2016-07-14 17:44:45 -0400

    assets in ignore

--- client/app/assets/asset-library.js
+++ client/app/assets/asset-library.js
@@ -1,3 +1,6 @@
+/**
+ * Created by Dmitriy Prilutsky on 14.07.2016.
+ */
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -8,110 +11,41 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-/**
- * Created by Dmitriy Prilutsky on 14.07.2016.
- */
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
 var assets_service_1 = require('../services/assets-service');
-var AssetsMain = (function () {
-    function AssetsMain(service) {
+var AssetLibrary = (function () {
+    function AssetLibrary(service) {
         this.service = service;
         this.cartItems = [new assets_service_1.Asset()];
     }
-    AssetsMain.prototype.ngOnInit = function () {
+    AssetLibrary.prototype.ngOnInit = function () {
         this.getData();
     };
-    AssetsMain.prototype.getData = function () {
+    AssetLibrary.prototype.getData = function () {
         var _this = this;
         this.service.getData()
             .subscribe(function (data) { return _this.data = data; }, function (error) { return _this.errorMessage = error; });
         {
         }
     };
-    AssetsMain.prototype.onClickItem = function (item) {
+    AssetLibrary.prototype.onClickItem = function (item) {
         this.fullItem = item;
     };
-    AssetsMain.prototype.hideFullImage = function () {
+    AssetLibrary.prototype.hideFullImage = function () {
         this.fullItem = null;
     };
-    AssetsMain.prototype.onDragEnd = function (evt) {
-        this.dragItem = null;
-    };
-    AssetsMain.prototype.onDragEnter = function (evt) {
-        this.toCart(this.dragItem);
-    };
-    AssetsMain.prototype.onDragStart = function (item) {
-        this.isMove = false;
-        this.dragItem = item;
-    };
-    AssetsMain.prototype.onDragOut = function (evt) {
-        if (!this.isMove)
-            this.offCart(this.dragItem);
-    };
-    AssetsMain.prototype.onSpacerDragEnter = function (item) {
-        var i = this.cartItems.indexOf(item);
-        this.insertToCardAt(this.dragItem, i);
-    };
-    AssetsMain.prototype.insertToCardAt = function (item, i) {
-        console.log(item, i, this.isMove);
-        if (item && i !== -1) {
-            if (this.isMove) {
-                var index = this.cartItems.indexOf(item);
-                if (index > -1) {
-                    this.cartItems.splice(index, 1);
-                }
-                this.cartItems.splice(i + 1, 0, item);
-            }
-            else {
-                if (i === (this.cartItems.length - 1))
-                    this.cartItems.push(item);
-                else
-                    this.cartItems.splice(i + 1, 0, item);
-            }
-            if (!this.isMove)
-                this.dragItem = null;
-        }
-    };
-    AssetsMain.prototype.toCart = function (item) {
-        if (item) {
-            this.cartItems.push(item);
-            var spacer = new assets_service_1.Asset();
-            spacer.spacer = true;
-        }
-    };
-    AssetsMain.prototype.offCart = function (item) {
-        if (item) {
-            var index = this.cartItems.indexOf(item);
-            console.log("offcart" + index);
-            if (index > -1) {
-                this.cartItems.splice(index, 1);
-            }
-        }
-    };
-    AssetsMain.prototype.onCartDragItemStart = function (item) {
-        this.isMove = true;
-        this.dragMove = item;
-        this.dragItem = item;
-    };
-    AssetsMain.prototype.onCartDragItemEnd = function (evt, item) {
-        if (this.isMove && evt.y > 300)
-            this.offCart(item);
-        this.isMove = false;
-        this.dragMove = null;
-        this.dragItem = null;
-    };
-    AssetsMain = __decorate([
+    AssetLibrary = __decorate([
         core_1.Component({
             selector: 'asset-library',
             template: "<div class=\"asset-library\">\n                     <md-content>\n                         <div class=\"card\" *ngFor=\"let item of data\">\n                            <md-card>\n                                      <img md-card-sm-image src=\" {{ item.thumb }} \" (click)=\"onClickItem(item)\">\n                            </md-card>\n                         </div>\n                     </md-content>\n                     <div class=\"full-image\" *ngIf=\"fullItem\"> \n                         <img src=\" {{ fullItem.img }} \" width=\"200\" (click)=\"hideFullImage()\">\n                     </div>\n               </div>\n                ",
-            styleUrls: ['app/assets/main.css'],
+            styles: ["\n                .card {\n                    height: 128px;\n                    width: 128px;\n                    float: left;\n                }\n            "],
             directives: [router_1.ROUTER_DIRECTIVES],
             providers: [assets_service_1.AssetsService]
         }), 
         __metadata('design:paramtypes', [assets_service_1.AssetsService])
-    ], AssetsMain);
-    return AssetsMain;
+    ], AssetLibrary);
+    return AssetLibrary;
 }());
-exports.AssetsMain = AssetsMain;
+exports.AssetLibrary = AssetLibrary;
 //# sourceMappingURL=asset-library.js.map
