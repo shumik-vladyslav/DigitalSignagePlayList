613551edec3b8147ba21853818d03d3208451d26
vlad <uplight.ca@gmail.com>
2016-07-15 11:59:10 -0400

    cdn

--- client/systemjs.config.js
+++ client/systemjs.config.js
@@ -4,16 +4,37 @@
  */
 (function(global) {
     // map tells the System loader where to look for things
+
+    var ngVer = '@2.0.0-rc.4'; // lock in the angular package version; do not let it float to current!
+    var routerVer = '@3.0.0-beta.1'; // lock router version
+    var formsVer = '@0.2.0'; // lock forms version
+    var routerDeprecatedVer = '@2.0.0-rc.2'; // temporarily until we update all the guides
+
+
+
     var map = {
         'app':                        'app', // 'dist',
-        '@angular':                   'node_modules/@angular',
+
+
+       /* '@angular':                   'node_modules/@angular',
+        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+        'rxjs':                       'node_modules/rxjs',
+*/
+
+        '@angular':                   'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
+        '@angular/router':            'https://npmcdn.com/@angular/router' + routerVer,
+        '@angular/forms':             'https://npmcdn.com/@angular/forms' + formsVer,
+        '@angular/router-deprecated': 'https://npmcdn.com/@angular/router-deprecated' + routerDeprecatedVer,
+        'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api', // get latest
+        'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
+
+
         '@angular2-material':         'node_modules/@angular2-material',
         'ng2-material':         'node_modules/ng2-material',
        // 'dragula': 'node_modules/dragula/dist/dragula.js',
        // 'ng2-dragula': 'node_modules/ng2-dragula',
-        'ng2-uploader':'node_modules/ng2-uploader',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        'rxjs':                       'node_modules/rxjs'
+        'ng2-uploader':'node_modules/ng2-uploader'
+
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
